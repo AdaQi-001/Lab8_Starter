7728f54627a70e63c1c5683c42f0d90a89578687
Ada Qi <97003885+AdaQi-001@users.noreply.github.com>
2023-05-28 23:40:54 -0700

    02

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lab 8 - Starter
+## Check Your Understanding question answers
+1. In my opinion, the automated tests in the Recipe project development pipeline should be integrated within a GitHub action that runs whenever code is pushed. This approach allows for immediate feedback on any issues or bugs introduced by code changes. By automatically running the tests in a CI/CD environment like GitHub actions, we can ensure continuous integration and catch any problems early in the development process.
+
+2. No, it wouldn't be appropriate to use an end-to-end test to check if a function is returning the correct output. End-to-end tests are more focused on verifying the overall behavior and interaction between different components of an application. Instead, we should use unit tests, which are specifically designed to test individual units of code, like functions, and are better suited for checking the correctness of function outputs.
+
+3. I would refrain from using a unit test to test the "message" feature of a messaging application. The "message" feature involves user interaction, network communication, and potential dependencies on other components. To ensure proper testing of this feature, it would be more appropriate to use integration tests or end-to-end tests. These tests can simulate user actions and interactions with the entire messaging system, providing more comprehensive coverage.
+
+4. Yes, a unit test would be suitable for testing the "max message length" feature of a messaging application. The "max message length" feature is typically a specific behavior of a single function or component. It can be effectively tested in isolation without relying on other parts of the system. By providing input messages of varying lengths, we can verify whether the function correctly enforces the maximum length constraint and returns the expected results.
+
+## Screenshots of test results
+![1](screenshots/01.png)
+![2](screenshots/02.png)
+![3](screenshots/03.png)
+![4](screenshots/04.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Test isPhoneNumber
+test('Valid phone number should return true', () => {
+  expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('Invalid phone number should return false', () => {
+  expect(functions.isPhoneNumber('123-456-789')).toBe(false);
+});
+
+test('Valid phone number without area code should return true', () => {
+  expect(functions.isPhoneNumber('456-7890')).toBe(true);
+});
+
+test('Invalid phone number with alphabets should return false', () => {
+  expect(functions.isPhoneNumber('(123) abc-7890')).toBe(false);
+});
+
+// Test isEmail
+test('Valid email should return true', () => {
+  expect(functions.isEmail('test@example.com')).toBe(true);
+});
+
+test('Invalid email should return false', () => {
+  expect(functions.isEmail('example.com')).toBe(false);
+});
+
+test('Valid email with underscore should return true', () => {
+  expect(functions.isEmail('test_email@example.com')).toBe(true);
+});
+
+test('Invalid email with special characters should return false', () => {
+  expect(functions.isEmail('test$@example.com')).toBe(false);
+});
+
+// Test isStrongPassword
+test('Valid strong password should return true', () => {
+  expect(functions.isStrongPassword('Abcdef123')).toBe(true);
+});
+
+test('Invalid weak password should return false', () => {
+  expect(functions.isStrongPassword('na')).toBe(false);
+});
+
+test('Valid strong password with underscore should return true', () => {
+  expect(functions.isStrongPassword('Strong_Pass123')).toBe(true);
+});
+
+test('Invalid password starting with a number should return false', () => {
+  expect(functions.isStrongPassword('1Password')).toBe(false);
+});
+
+// Test isDate
+test('Valid date should return true', () => {
+  expect(functions.isDate('05/28/2023')).toBe(true);
+});
+
+test('Invalid date should return false', () => {
+  expect(functions.isDate('2023/05/28')).toBe(false);
+});
+
+test('Valid date with single-digit day and month should return true', () => {
+  expect(functions.isDate('5/8/2023')).toBe(true);
+});
+
+test('Invalid date with extra digits should return false', () => {
+  expect(functions.isDate('05/28/20230')).toBe(false);
+});
+
+// Test isHexColor
+test('Valid 3-digit hex color should return true', () => {
+  expect(functions.isHexColor('#abc')).toBe(true);
+});
+
+test('Invalid hex color should return false', () => {
+  expect(functions.isHexColor('ff00gg')).toBe(false);
+});
+
+test('Valid 6-digit hex color should return true', () => {
+  expect(functions.isHexColor('#aabbcc')).toBe(true);
+});
+
+test('Invalid hex color with extra digits should return false', () => {
+  expect(functions.isHexColor('#abcdef00')).toBe(false);
+});
